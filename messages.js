9cb62a3314e3c87f4bdd05e8157cd81bae0f75dd
Aka Si Anse <67917871+PrimeKnight4@users.noreply.github.com>
2024-07-28 13:19:01 +0800

    Add files via upload

---
+++ messages.js
@@ -0,0 +1,261 @@
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+  surg: stripIndents `♨️SELL CHEAP AT TUKU♨️
+  
+ANESTHETIC 15/1
+SPONGE 15/1
+ANTIBIOTIC 14/1
+SCALPEL 15/1
+PINS 17/1
+SPLINT 16/1
+LAB KIT 17/1
+TRANSFUSION 16/1 
+ULTRASOUND 16/1
+DEFIBRILLATOR 17/1
+ANTISEPTIC 16/1
+CLAMP 17/1
+STITCHES 200/118 CAN DM @BOSTIKTOK 
+SURG E 4/1
+MED A CHECK 
+TEA 200/1 
+BONE SAW 35
+SKILL SPICE 
+HEALING TOME 9
+  
+ ♨️SELL CHEAP AT TUKU♨️`,
+  
+  food: stripIndents `♨️SELL CHEAP AT TUKU♨️
+  
+  ARROZ CAN POLLO 3/1
+  
+  ♨️SELL CHEAP AT TUKU♨️`,
+  
+ bow: stripIndents `♨️SELL CHEAP AT TUKU♨️
+BOW 🎴
+❤️‍HEARTBOW 60
+💥ORB OF ELIXIR 25
+🤩ETRACTOR SNAP 17
+SLITHERING SLIPPERS 380
+COSMIC CAPE 130
+DARK FAIRY PENDANT 20
+HELPING HAND 20
+WARVING INFLATABLE ARM GUY 18
+TENNY ANGEL WINGS 6
+CARE PEAR 5
+ICE SERPENT SCARF 5
+
+♨️SELL CHEAP AT TUKU♨️`,
+  
+  blast: stripIndents `♨️SELL CHEAP AT TUKU♨️
+  BLAST🏞
+  TREASURE BLAST 6
+  CAVE BLAST 18
+  
+  ♨️SELL CHEAP AT TUKU♨️`,
+  
+  
+  jammer: stripIndents`♨️SELL CHEAP AT TUKU♨️
+JAMMERS🏛
+CCTV 7
+BIRTH SERFITICATE 16 
+PIEGON 6
+Signal Jammer (SJ) 2
+Punch Jammer (PJ) 9
+Zombie Jammer (ZJ) 9
+FIREHOUSE 4
+ADDRES 14 
+GCHARM 33
+SECURTY CAMERA 6
+AG 240
+MINIMOD 80
+Save Fault 6
+Storage Box LVL 125
+ELDRITCH FLAME 4/1
+Pocket Lighter 3/1
+♨️SELL CHEAP AT TUKU♨️`,
+  
+  lock: stripIndents`♨️SELL CHEAP AT TUKU♨️
+
+LOCK 🤑
+👷BUILDER LOCK 33
+🤩HUGE LOCK (HL) 2-3/1
+🥳SMALL LOCK (SL) 28/1
+😋Big Lock (BL) 6-7/1
+LOCK OF BLACK FUR 2
+🐰BUNNY LOCK 20
+👻ECTO LOCK 15
+LOCK BOT REMOTE 13
+RAYMAN LOCK 85
+ROBOTIC LOCK 120
+🐉LEGENDARY LOCK (LL) 160
+🐲BLOOD DRAGON LOCK (BLOD) 230
+🏜PRINCE OF PERSIA LOCK (PERSIA) 45
+R ENCHANTED LOCK 
+🤐LOCK MOVER 21
+
+
+♨️SELL CHEAP AT TUKU♨️`,
+  
+  weather: stripIndents`♨️SELL CHEAP AT TUKU♨️
+WEATHER  🌍
+weather nothingness
+weather infinity 25 wl
+weather stuff 6 wl 
+weather night 5 wl
+weather stargazing 5 wl
+weather rainy city 5 wl 
+weather spooky 6 wl
+weather snowy 13 wl
+weather infinity 20 wl
+weather st paddy 20 wl
+weather digital rain 20 wl
+weather valentines 25 wl 
+weather jungle 12 wl
+weather plaza 50 wl
+weather celebriry hills 45 wl 
+weather crack in reality 50 wl
+weather dark mountains 60 wl
+weather background 105 wl
+weather autumn 120 wl 
+weather frozen cliffs 215 wl 
+weather nian mountains 335 wl
+weather realm of spirits 700 wl
+♨️SELL CHEAP AT TUKU♨️`,
+  
+  locke: stripIndents`♨️SELL CHEAP AT TUKU♨️
+  
+  DOOR VIP 27
+  
+    ♨️SELL CHEAP AT TUKU♨️`,
+  
+  plat: stripIndents`♨️SELL CHEAP AT TUKU♨️
+
+ FIRE ESCAPE PLATFORM 45/1
+ HOVER PLATFORM 5/1
+ CRAKCED STONE SLAB 3
+ HORIZONTACLES 10/1
+ SEAGULL 10/1
+ TAVERN SIGN 3/1
+ GREETINGS EARTHLINGS SIGN 15
+ SPACE STATION SIGN 4
+ SUBWAY SIGN 3
+ SPACE CONYEVOR 4
+ STEERT CLOCK 2
+ FROZEN FOUNTAIN 3
+ PIZZA SIGN 1
+ OPEN SIGN 9/1
+ TAKE AWAY JOINT 9/1 
+ TAKE AWAY JOINT ROOF 5/1
+
+♨️SELL CHEAP AT TUKU♨️`,
+  
+  door: stripIndents`♨️SELL CHEAP AT TUKU♨️
+  
+BLUE PORTAL 11/1
+HELLGATE PORTAL 4/1
+LOVE PORTAL 2/1  
+FORCEFIELD 2/1
+DARK MAGIC BARRIER 1/1
+OBJECTIVE MARKER 5/1
+The Adventure Begins 3/1
+Flower Checkpoint 5/1
+TIME SPACE RUPTURE PORTAL 3/1
+MYSTERY PORTAL 3/1
+ORANGE PORTAL 2/1
+GUILD ENTRANCE  ORNATE 3
+GUILD ENTRANCE NORMAL 3
+GATE AWAY ADVENTURE 5/1
+PASSWORD DOOR 4/1
+DRAGON GATE 11/1
+STREET SIDEWALK 3/1
+SUBWAY STAIRS 2
+BAROQUE IRON FENCE 2/1
+  
+ ♨️SELL CHEAP AT TUKU♨️`,
+  
+  stem: stripIndents`♨️SELL CHEAP AT TUKU♨️
+  
+TUBES 13/1
+FUNNEL 9/1
+STEAM DOOR 2/1
+SPROCKET 19
+PISTON 11/1
+COLLCETOR 13/1
+FUNNEL - UP 10/1
+CROSSOVER 10/1
+LIFFER PISTON 12/1
+BELLOWS 11/1
+  
+ ♨️SELL CHEAP AT TUKU♨️`,
+  
+ paint: stripIndents`♨️SELL CHEAP AT TUKU♨️
+PAINT 🥳
+🔴Paint Bucket - Red 8/1
+🟣Paint Bucket - Purple 8/1
+🟢Paint Bucket - Green 17/1
+⚫Paint Bucket - Charcoal 7/1
+🔵Paint Bucket - Blue 16/1
+🔵Paint Bucket - Aqua  18/1
+⚪Paint Bucket - Varnish 18/1
+🟡Paint Bucket - Yellow  18/1
+🖌️PAINTBRUSH 25
+😎PORTRAIT 15
+PAINTING EASEL 28
+♨️SELL CHEAP AT TUKU♨️`,
+  
+  backg: stripIndents`♨️SELL CHEAP AT TUKU♨️
+
+MANOR HOUSE SANDSTONE BACK 1/1
+Lotus Lamp 1/1
+Lucky Clover Background 25/1
+Maneki-Neko 2
+Superstar Background 20
+COUCH 11/1
+BLACK LEATHER COUCH 3
+DINER COUCH 2
+GARDEN BENCH 1
+BASKET CHAIR 6
+PARTY COUCH 1
+DRAGON THRONE 10
+THE PINE THRONE 4
+ICE THRONE OF WINTER 4
+TERRACOTTA 1
+URANIUM CHAIR 40
+HAND CHAIR 2/1
+BURRITO BRONCHO CHAIR 8
+SUBWAY WALL 8/1
+STORM DRAIN 10/1
+  
+  ♨️SELL CHEAP AT TUKU♨️`,
+  
+  block: stripIndents `♨️SELL CHEAP AT TUKU♨️
+BLOCK 🧱
+DISPLAY BLOCK 2/1
+DISPLAY BOX 5/1
+NUMBER BLOCK 1-9 6/1
+BLACK DISPLAY SHELF 4
+XENOID BLOCK 15/1
+PILLAR 6/1
+CLOUDS 10/1
+ALL SHINING BLOCK 4/1
+CELTIC BLOCK 30/1 
+HEDGE 105/1
+TRANSMATTER 2/1
+FIRE PLACE 6/1
+ONE WAT BLOCK 2/1
+STAINED GLASS 3/1
+MANEEQUIN 3/1 & ALL Mane
+REGAL STAIRS 7/1
+REGAL BANNISTER 60/1
+CASTLE STONE 2/1
+TOPIARY HEDGE 1/1
+UP AND AWAY BLOCK 1/1
+SLICK BLOCK 150/1
+BASIC BLUE BLOCK 3/1
+GUEST BOOK 5/1
+TECHNO GRASS 2/1
+DIGITAL DIRT 3/1
+♨️CHEAP ALL GO TUKU FAMOUS WORLD♨️`
+  
+}
